059a9b01a7cbed64a75be7015000abe70ebf3072
Mauricio Coelho <maudrico9@gmail.com>
2023-05-02 00:42:23 -0300

    update

--- resources/views/layouts/portal.blade.php
+++ resources/views/layouts/portal.blade.php
@@ -5,9 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="description" content="Mauricio Coelho">
         <meta name="author" content="Mauricio Coelho">
-        <link rel="apple-touch-icon" sizes="180x180" href="{{asset('assets-admin/assets/images/favicon//apple-touch-icon.png')}}">
-        <link rel="icon" type="image/png" sizes="32x32" href="{{asset('assets-admin/assets/images/favicon//favicon-32x32.png')}}">
-        <link rel="icon" type="image/png" sizes="16x16" href="{{asset('assets-admin/assets/images/favicon//favicon-16x16.png')}}">
+        <link rel="apple-touch-icon" sizes="180x180" href="{{asset('assets-admin/assets/images/favicon_io/apple-touch-icon.png')}}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{asset('assets-admin/assets/images/favicon_io/favicon-32x32.png')}}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{asset('assets-admin/assets/images/favicon_io/favicon-16x16.png')}}">
         <title>{{ config('app.name') }} &mdash; @yield('title')</title>
         <!-- Simple bar CSS -->
 
